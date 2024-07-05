c7ddd7593cb4d760c3261e705045467084dfe85b
Abhishek Yadav <abhishekay442080@gmail.com>
2024-07-05 22:03:42 +0530

    Completed sum of list problem

--- sumOfElements.py
+++ sumOfElements.py
@@ -1,1 +1,10 @@
 # Write a Python program to find the sum of all elements in a list.
+
+def sum_of_elements(lst):
+    return sum(lst)
+
+
+nmb = [1, 2, 3, 4, 5]
+tSum = sum_of_elements(nmb)
+
+print(f"The sum of all elements in the list is: {tSum}")
